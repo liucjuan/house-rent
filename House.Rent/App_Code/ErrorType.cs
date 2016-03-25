f8018c0f1973f6fdea1420cfcf96de2f26d911d2
lcj08996 <lcj08996@ly.com>
2016-03-25 16:51:12 +0800

    代码修改~

---
+++ House.Rent/App_Code/ErrorType.cs
@@ -0,0 +1,26 @@
+﻿using BirdSof;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Web;
+
+/// <summary>
+/// ErrorType 的摘要说明
+/// </summary>
+public enum ErrorType
+{
+    [EnumDescription("成功")]
+    Success = 0000,
+
+    [EnumDescription("失败")]
+    Failed = 0001,
+
+    [EnumDescription("当前用户不存在")]
+    BadUser = 0002,
+
+    [EnumDescription("密码有误")]
+    BadPassWord = 0003,
+}
+
+
+// string txt = EnumDescription.GetFieldText(ErrorType.BadPassWord);
---
+++ House.Rent/App_Code/TableProClsHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Web;
+
+/// <summary>
+/// TableProClsHelper 的摘要说明
+/// </summary>
+public class TableProClsHelper
+{
+    public DataSet GetPro_ClsInfo(string pro_cls_id)
+    {
+        if (!string.IsNullOrEmpty(pro_cls_id))
+        {
+            StringBuilder sql = new StringBuilder();
+            sql.AppendFormat("select * from pro_cls where pro_cls_id=", DBConfig.pro_cls, pro_cls_id);
+            DataSet dataSet = DBHelper.GetDataSet(sql.ToString());
+            if (dataSet != null)
+            {
+                return dataSet;
+            }
+        }
+        return new DataSet();
+    }
+
+    public string GetPro_ClsFields(string fields, string pro_cls_id)
+    {
+        string value = string.Empty;
+        DataSet dataSet = GetPro_ClsInfo(pro_cls_id);
+        if (dataSet != null)
+        {
+            value = dataSet.Tables[0].Rows[0][fields].ToString();
+            if (!string.IsNullOrEmpty(value))
+            {
+                return value;
+            }
+        }
+        return string.Empty;
+    }
+}
